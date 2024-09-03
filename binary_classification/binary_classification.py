bc4d4a5c68ae4556492b7c95d2251913835a66f5
Pedro Sartori <pedrosartdr@gmail.com>
2024-09-03 02:36:03 -0300

    trains the model for binary classification

--- binary_classification/binary_classification.py
+++ binary_classification/binary_classification.py
@@ -4,6 +4,7 @@ import torch.nn.functional as f
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 np.random.seed(123)
 torch.manual_seed(123)
@@ -41,4 +42,27 @@ net = Net()
 criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(net.parameters(), 0.001, weight_decay=0.0001)
 
-net(xtrain)
+
+for epoch in range(1000):
+    running_loss = 0.0
+    for data in train_loader:
+        inputs, labels = data
+        
+        optimizer.zero_grad()
+        
+        outputs = net(inputs)
+        
+        loss = criterion(outputs, labels)
+        # loss = ((outputs - labels) ** 2.0).sum()
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss
+        
+    running_loss /= len(train_loader)
+    print(running_loss)
+
+ypred = torch.tensor([1 if x > 0.5 else 0 for x in net(xtest)], dtype=torch.int32)
+ypred = ypred.unsqueeze(-1)
+
+print(accuracy_score(ytest, ypred))
