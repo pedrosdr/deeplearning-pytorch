e3867c27ed7436d7372e5781af98a215d86cd7dc
Pedro Sartori <pedrosartdr@gmail.com>
2025-02-21 00:26:56 -0300

    Extracts the features of the VGG19 model and creates a function to calculate the gram_matrix of these features

--- 18_style_transfer/style_transfer.py
+++ 18_style_transfer/style_transfer.py
@@ -30,11 +30,45 @@ style_img = transform(style_img)
 new_img = new_img.unsqueeze(0)
 style_img = style_img.unsqueeze(0)
 
+
 def plot(img):
     img = img.to(cpu).clone().detach().numpy().squeeze()
     img = img.transpose(1,2,0)
     img = img * np.array([0.229, 0.224, 0.225]) + np.array([0.485, 0.456, 0.406])
     plt.imshow(img)
     
+    
 plot(style_img)
 plot(new_img)
+
+
+def extract_features(image):
+    layers = [0, 5, 10, 19, 28]
+    
+    features = {}
+    
+    for n in range(len(model.features)):
+        image = model.features[n](image)
+        if n in layers:
+            features[n] = image
+            
+    return features
+
+new_img = new_img.to(device)
+style_img = style_img.to(device)
+
+features_new_img = extract_features(new_img)
+features_style_img = extract_features(style_img)
+
+
+def gram_matrix(tensor):
+    b, c, h, w = tensor.size()
+    tensor = tensor.view(c, h*w)
+    
+    gram = torch.mm(tensor, tensor.t())
+    return gram
+
+
+style_img_grams = {
+    layer: gram_matrix(feature) for layer, feature in features_style_img.items()
+}
